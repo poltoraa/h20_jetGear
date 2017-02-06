de7bcb611c438cd14c4e9161fdc48f1e17f1ab2e
itxor <poltishru@gmail.com>
2017-02-06 17:26:45 +0700

    Add to cart the input field for the coupon

--- .default/components/bitrix/sale.basket.basket/cart/template.php
+++ .default/components/bitrix/sale.basket.basket/cart/template.php
@@ -1,4 +1,4 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -14,81 +14,69 @@
 /*echo "<pre>";
 print_r($arResult["ITEMS"]["AnDelCanBuy"]);
 echo "</pre>";*/
-$econom=0;
+$econom = 0;
 ?>
 <!-- BEGIN BASKET BLOCK -->
 
-    <div class="basket_main_block">
+<div class="basket_main_block">
 
-        <div class="container">
+    <div class="container">
 
 
+        <div class="row">
 
-            <div class="row">
-            
             <?
-            if(count($arResult["ITEMS"]["AnDelCanBuy"])>0)
+            if (count($arResult["ITEMS"]["AnDelCanBuy"]) > 0)
             {
             ?>
 
 
-
-                <h2>Корзина</h2>
-
+            <h2>Корзина</h2>
 
 
-                <div class="row">
+            <div class="row">
 
 
-
-                    <div class="col-md-9">
-
+                <div class="col-md-9">
 
 
-                        <div class="order_block">
-                        
+                    <div class="order_block">
+
                         <?
-                        foreach ($arResult["ITEMS"]["AnDelCanBuy"] as $k => $arItem)
-                        {
-                        ?>
-
+                        foreach ($arResult["ITEMS"]["AnDelCanBuy"] as $k => $arItem) {
+                            ?>
 
 
                             <div class="order_row clearfix">
 
 
-
                                 <div class="order_img">
 
-                                    <img src="<?=$arItem["PREVIEW_PICTURE_SRC"]?>" width="85" alt="<?=$arItem["NAME"]?>">
+                                    <img src="<?= $arItem["PREVIEW_PICTURE_SRC"] ?>" width="85"
+                                         alt="<?= $arItem["NAME"] ?>">
 
                                 </div>
-
-
-
 
 
                                 <div class="order_txt_block">
 
 
+                                    <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
 
-                                    <a href="<?=$arItem["DETAIL_PAGE_URL"] ?>">
-
-                                        <h6><?=$arItem["NAME"]?></h6>
+                                        <h6><?= $arItem["NAME"] ?></h6>
 
                                     </a>
 
 
-
                                     <div class="star_id_block">
-
 
 
                                         <div class="rating_block clearfix">
 
                                             <ul class="star_rating_nopick">
 
-                                                <li class="current" style="width: <?=($arItem["RATE"]*20)?>%"><span class="star1" title="Рейтинг 1 из 5"></span></li>
+                                                <li class="current" style="width: <?= ($arItem["RATE"] * 20) ?>%"><span
+                                                        class="star1" title="Рейтинг 1 из 5"></span></li>
 
                                                 <li><span class="star2" title="Рейтинг 2 из 5"></span></li>
 
@@ -100,193 +88,176 @@ $econom=0;
 
                                             </ul>
 
-                                            <a>(<?=$arItem["NUM"]?>)</a>
+                                            <a>(<?= $arItem["NUM"] ?>)</a>
 
                                         </div>
-
 
 
                                         <div class="id_block">
 
-                                            <b>ID:</b> <span><?=$arItem["PROPERTY_ID_VALUE"]?></span>
+                                            <b>ID:</b> <span><?= $arItem["PROPERTY_ID_VALUE"] ?></span>
 
                                         </div>
-
 
 
                                     </div>
 
 
-
                                 </div>
-
 
 
                                 <div class="order_number">
 
 
-
                                     <span class="minus"><i class="fa fa-minus" aria-hidden="true"></i></span>
 
-                                    <input class="good_count" type="text" value="<?=$arItem["QUANTITY"]?>" rel="<?=$arItem["ID"]?>">
+                                    <input class="good_count" type="text" value="<?= $arItem["QUANTITY"] ?>"
+                                           rel="<?= $arItem["ID"] ?>">
 
                                     <span class="plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
 
 
-
-                                    <div class="delete_block" rel="<?=$arItem["ID"]?>"><span>Удалить</span></div>
-
+                                    <div class="delete_block" rel="<?= $arItem["ID"] ?>"><span>Удалить</span></div>
 
 
                                 </div>
-
 
 
                                 <div class="price_block">
-<?
+                                    <?
 
-if(!empty($arItem["DISCOUNT_PRICE"]) && $arItem["PRICE"]<$arItem["FULL_PRICE"])
-{
-?>
-                                    <div class="old_price"><?=$arItem["FULL_PRICE_FORMATED"]?></div>
+                                    if (!empty($arItem["DISCOUNT_PRICE"]) && $arItem["PRICE"] < $arItem["FULL_PRICE"]) {
+                                        ?>
+                                        <div class="old_price"><?= $arItem["FULL_PRICE_FORMATED"] ?></div>
 
-                                    <div class="new_price"><?=$arItem["PRICE_FORMATED"]?></div>
-<?}
-else 
-{
-	?>
-	 <div class="new_price"><?=$arItem["PRICE_FORMATED"]?></div>
-	<?
-}
-?>
+                                        <div class="new_price"><?= $arItem["PRICE_FORMATED"] ?></div>
+                                        <?
+                                    } else {
+                                        ?>
+                                        <div class="new_price"><?= $arItem["PRICE_FORMATED"] ?></div>
+                                        <?
+                                    }
+                                    ?>
 
-<?
-if(!empty($arItem["DISCOUNT_PRICE"]) && $arItem["PRICE"]<$arItem["FULL_PRICE"])
-{
-	$econom=($arItem["FULL_PRICE"]-$arItem["DISCOUNT_PRICE"])*$arItem["QUANTITY"];
-?>
-                                    <div class="economy_block">
+                                    <?
+                                    if (!empty($arItem["DISCOUNT_PRICE"]) && $arItem["PRICE"] < $arItem["FULL_PRICE"]) {
+                                        $econom = ($arItem["FULL_PRICE"] - $arItem["DISCOUNT_PRICE"]) * $arItem["QUANTITY"];
+                                        ?>
+                                        <div class="economy_block">
 
-                                        <span>Экономия <?=CurrencyFormat(($arItem["FULL_PRICE"]-$arItem["DISCOUNT_PRICE"]),"RUB")?> </span>
+                                            <span>Экономия <?= CurrencyFormat(($arItem["FULL_PRICE"] - $arItem["DISCOUNT_PRICE"]), "RUB") ?> </span>
 
-                                    </div>
-                                    <?}?>
+                                        </div>
+                                    <? } ?>
 
                                 </div>
-
 
 
                             </div>
 
-<?}?>
-
-                            
-
-
-
-                        </div>
-
-
-
+                        <? } ?>
 
 
                     </div>
 
 
-
-                    <div class="col-md-3">
-
-
-
-                        <div class="billing_block">
-
-
-
-                            <div class="billing_row">
-
-
-
-                                <div class="price_block">
-
-                                    <div class="new_price">
-
-                                        <h2><?=str_replace("руб.", "", $arResult["allSum_FORMATED"])?></h2> руб.</div>
-<?
-if(!empty($arResult["PRICE_WITHOUT_DISCOUNT"]) && intval($econom)>0)
-{
-?>
-                                    <div class="old_price"><?=$arResult["PRICE_WITHOUT_DISCOUNT"]?></div>
-                                    <?}?>
-
+                    <?global $USER;
+                        if ($USER->isAdmin())
+                        {
+                            ?>
+                            <div class="container" style="width: 676px;margin-top: 27px;position: absolute;/* margin-bottom: 20px; */">
+                                <div class="subscribe_txt_block" style="margin-top: 11px;">
+                                    <h6>Введите купон:</h6>
                                 </div>
 
-<?
-if(intval($econom)>0)
-{
-?>
-
-                                <span>Экономия <?=CurrencyFormat($econom,"RUB")?></span>
-                                
-                                <?}?>
-
-
-
+                                <form action="" method="POST" class="subscr_form_1" style="float: right;">
+                                    <input type="text" placeholder="Введите код на скидку" name="EMAIL" required="" style="/* font-size: 14px; */padding-left: 20px; width: 427px; line-height: 44px; font-size: 16px; border: none; outline: 1px solid #dfe6e9;" >
+                                </form>
                             </div>
-
-
-
-                            <div class="billing_row">
-
-
-
-                                <p>Доставим курьером</p>
-
-
-
-                                <div class="txt_line">
-
-                                    <h6>Сегодня</h6>
-
-                                    <p>за</p>
-
-                                    <h6><?=CurrencyFormat($arResult["DELIVERY"]["PRICE"], $arResult["DELIVERY"]["CURRENCY"])?></h6>
-
-                                </div>
-
-
-
-                            </div>
-
-
-
-                            <div class="billing_row">
-
-                                <a href="/personal/order/" class="btn blue_btn">Оформить</a>
-
-                            </div>
-
-
-
-                        </div>
-
-
-
-                    </div>
-
-<?}?>
+                            <?
+                        }
+                    ?>
 
                 </div>
 
 
+                <div class="col-md-3">
 
 
+                    <div class="billing_block">
+
+
+                        <div class="billing_row">
+
+
+                            <div class="price_block">
+
+                                <div class="new_price">
+
+                                    <h2><?= str_replace("руб.", "", $arResult["allSum_FORMATED"]) ?></h2> руб.
+                                </div>
+                                <?
+                                if (!empty($arResult["PRICE_WITHOUT_DISCOUNT"]) && intval($econom) > 0) {
+                                    ?>
+                                    <div class="old_price"><?= $arResult["PRICE_WITHOUT_DISCOUNT"] ?></div>
+                                <? } ?>
+
+                            </div>
+
+                            <?
+                            if (intval($econom) > 0) {
+                                ?>
+
+                                <span>Экономия <?= CurrencyFormat($econom, "RUB") ?></span>
+
+                            <? } ?>
+
+
+                        </div>
+
+
+                        <div class="billing_row">
+
+
+                            <p>Доставим курьером</p>
+
+
+                            <div class="txt_line">
+
+                                <h6>Сегодня</h6>
+
+                                <p>за</p>
+
+                                <h6><?= CurrencyFormat($arResult["DELIVERY"]["PRICE"], $arResult["DELIVERY"]["CURRENCY"]) ?></h6>
+
+                            </div>
+
+
+                        </div>
+
+
+                        <div class="billing_row">
+
+                            <a href="/personal/order/" class="btn blue_btn">Оформить</a>
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+                <? } ?>
 
             </div>
 
 
-
         </div>
+
 
     </div>
 
-    <!-- END BASKET BLOCK -->
+</div>
+
+<!-- END BASKET BLOCK -->
